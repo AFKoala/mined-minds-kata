52611a3d16d1f0dc4755bbde9636de58a600ddfe
AFKoala <bubbles.murph@gmail.com>
2017-06-21 08:03:26 -0400

    June 20 Homework

--- ex4.rb
+++ ex4.rb
@@ -2,10 +2,12 @@ count = 0
 while count < 100
 	count += 1
 	# Replaces multiples of 3 with Mined
-	if count % 3 == 0
-		puts "Mined"
+	if count % 3 == 0 and count % 5 == 0
+		puts "Mined Minds"
 	elsif count % 5 == 0
 		puts "Minds"
+	elsif count % 3 == 0 
+		puts "Mined"			
 	else
 		puts count
 	end
