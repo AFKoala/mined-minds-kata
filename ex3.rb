eb37c361e01ffb375279ff0c28646e91799b54ed
AFKoala <bubbles.murph@gmail.com>
2017-06-22 11:00:00 -0400

    June 20 Homework

--- ex3.rb
+++ ex3.rb
@@ -4,7 +4,8 @@ while count < 100
 	# Replaces multiples of 3 with Mined
 	if count % 3 == 0
 		puts "Mined"
-	elsif count == 5
+	# Replaces multiples of 5 with Minds
+	elsif count % 5 == 0
 		puts "Minds"
 	else
 		puts count
